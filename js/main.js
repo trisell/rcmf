03f8d6fdff3c708e4bf160ed2729ae433718182c
Dom <ddesimini@gmail.com>
2016-08-30 12:41:31 -0600

    Updating links

--- js/main.js
+++ js/main.js
@@ -1,55 +1,57 @@
 $(document).ready(function(){
-    $('.carousel').carousel({
-      interval: 2000
+        $('.carousel').carousel({
+        interval: 2000
+        });
+    //$("#top").parallaxify();
+
+
+    /*// Validating Empty Field
+    function check_empty() {
+    if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+    alert("Fill All Fields !");
+    } else {
+    document.getElementById('form').submit();
+    alert("Form Submitted Successfully...");
+    }
+    }
+    //Function To Display Popup
+    function div_show() {
+    document.getElementById('abc').style.display = "block";
+    }
+    //Function to Hide Popup
+    function div_hide(){
+    document.getElementById('abc').style.display = "none";
+
+    }
+    */
+    $("#form").click(function(){
+        $("#golfevent").modal('hide'); //hide popup
     });
-  //$("#top").parallaxify();
 
+    $("#TVform").click(function(){
+        $("#tvgolfevent").modal('hide'); //hide popup
+    });
+    $("#TVpokerform").click(function(){
+        $("#tvgolfevent").modal('hide'); //hide popup
+    });
+    $('#watch-me').change(function(){
+            if(this.checked)
+                $('#show-me').show();
+            else
+                $('#show-me').hide();
 
-/*// Validating Empty Field
-function check_empty() {
-if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
-alert("Fill All Fields !");
-} else {
-document.getElementById('form').submit();
-alert("Form Submitted Successfully...");
-}
-}
-//Function To Display Popup
-function div_show() {
-document.getElementById('abc').style.display = "block";
-}
-//Function to Hide Popup
-function div_hide(){
-document.getElementById('abc').style.display = "none";
+    });
+    $('#tvwatch-me').change(function(){
+            if(this.checked)
+                $('#tvshow-me').show();
+            else
+                $('#tvshow-me').hide();
 
-}
-*/
-$("#form").click(function(){
-    $("#golfevent").modal('hide'); //hide popup
+    });
+    $("#fvideo").on('hidden.bs.modal', function (e) {
+        $("#fvideo iframe").attr("src", $("#fvideo iframe").attr("src"));
+    });
 });
-
-$("#TVform").click(function(){
-    $("#tvgolfevent").modal('hide'); //hide popup
-});
-$("#TVpokerform").click(function(){
-    $("#tvgolfevent").modal('hide'); //hide popup
-});
-$('#watch-me').change(function(){
-        if(this.checked)
-            $('#show-me').show();
-        else
-            $('#show-me').hide();
-
-});
-$('#tvwatch-me').change(function(){
-        if(this.checked)
-            $('#tvshow-me').show();
-        else
-            $('#tvshow-me').hide();
-
-});
-
-
 $(document).ready(function () {
     var target = document.location.hash.replace("#", "");
     if (target.length) {
@@ -83,6 +85,7 @@ $(document).ready(function () {
     else{}
 
 	};
+    
 });
 function printDiv(divName) {
  alert('s');
@@ -92,4 +95,3 @@ function printDiv(divName) {
  w.print();
  w.close();
 };
-});
